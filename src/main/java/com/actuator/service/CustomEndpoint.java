258fc08d5ffbdb8bfb28276a71ac911916305f64
rajasekhar1404 <mrrajasekhar09@gmail.com>
2023-03-12 19:44:31 +0530

    spring actuator demo

--- src/main/java/com/actuator/service/CustomEndpoint.java
+++ src/main/java/com/actuator/service/CustomEndpoint.java
@@ -1,2 +1,21 @@
-package com.actuator.service;public class CustomEndpoint {
+package com.actuator.service;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Endpoint(id = "dashboard")
+@Component
+public class CustomEndpoint {
+
+    @ReadOperation
+    public Map<String, String> getDashboard(String username) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("dashboard", "welcome " + username);
+        return map;
+    }
+
 }
--- src/main/java/com/actuator/service/DataService.java
+++ src/main/java/com/actuator/service/DataService.java
@@ -1,2 +1,21 @@
-package com.actuator.service;public class DataService {
+package com.actuator.service;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataService implements HealthIndicator {
+    @Override
+    public Health health() {
+        if(isDataRunning()) {
+            return Health.up().withDetail("app", "demo").build();
+        }
+        return Health.down().withDetail("app", "demo").build();
+    }
+
+    public boolean isDataRunning() {
+        return true;
+    }
+
 }
